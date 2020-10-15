1da657e36d2bfe7ba1ce8e64cd9965ba23eace00
JuliaSleptsova <yuli.sleptsova@gmail.com>
2020-10-15 14:51:14 +0300

    updated test, added ArayList

--- src/test/java/com/belkaCoder/noteDream/AppTest.java
+++ src/test/java/com/belkaCoder/noteDream/AppTest.java
@@ -1,6 +1,8 @@
 package com.belkaCoder.noteDream;
 
+import com.belkaCoder.noteDream.NoteMap;
 import org.apache.commons.lang3.RandomStringUtils;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -9,20 +11,21 @@ import java.io.IOException;
 /**
  * Unit test for simple App.
 */
-public class AppTest 
-{
-
+public class AppTest {
 
     @Test
     public void initTest() throws IOException {
         var note = new NoteMap();
         var list = note.createList();
-        int date = 0;
+        ArrayList<String> mass = new ArrayList<>();
+        String data = "15.10.2020";
         for (int i = 1; i < 21; i++) {
-            String generatedString = RandomStringUtils.randomAlphabetic(10);
-            note.addList(i,  generatedString);
+            String task = i + ". " + RandomStringUtils.randomAlphabetic(10);
+            mass.add(task);
+
         }
-        note.printList(list, date);
+        note.addList(data, mass);
+        note.printList(list);
     }
 
 
